4ec3fba233b294f8ded787a01cccf512bec7bba7
AlainKRSS <cvt.alain@gmail.com>
2019-02-25 23:09:49 +0100

    fix(icons): correct svg

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
 .Icon
   router-link.Icon-link(:to="{ name: 'projects', params: { color: color, shape: shape } }")
-  svg.Icon-shape(@mousedown="mouseDown" @mouseup="mouseUp" width="37px" height="36px" viewBox="0 0 37 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink")
+  svg.Icon-shape(@mousedown="mouseDown" @mouseup="mouseUp" width="40px" height="40px" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink")
     defs
       g#shape
-        circle(v-if="shape === 'circle'" cx="19.5" cy="19.5" r="17")
-        polygon(v-if="shape === 'triangle'" transform="translate(19.360294, 28.493055) rotate(136.000000) translate(-19.360294, -28.493055) " points="11.1624486 11.1720037 36.6712247 35.5739934 2.04936286 45.8141065")
-        rect(v-if="shape === 'square'" x="2.5" y="2.5" width="34" height="34")
-        path(v-if="shape === 'pentagone'" d="M19.5,2.61803399 L1.54218318,15.6651516 L8.40145884,36.7758314 L30.5985412,36.7758314 L37.4578168,15.6651516 L19.5,2.61803399 Z")
-    g(transform="translate(-1, -2)")
+        circle(v-if="shape === 'circle'" cx="20" cy="20" r="18.8")
+        polygon(v-if="shape === 'triangle'" points="20,1.1 1.2,33.6 38.8,33.6")
+        rect(v-if="shape === 'square'" x="1.2" y="1.2" width="37.5" height="37.5")
+        polygon(v-if="shape === 'pentagone'" points="20,1.2 1.2,14.8 8.4,36.9 31.6,36.9 38.8,14.8")
+    g
       g.Icon-strokes
         use(href="#shape")
       g.Icon-fill(v-bind:class="isClicking ? 'is-active' : ''")
