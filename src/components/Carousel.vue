cc8b27ed9ae556ae392daa7b119113f22522aa83
AlainKRSS <cvt.alain@gmail.com>
2019-03-06 19:26:06 +0100

    fix(): fix initil shape

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -3,16 +3,18 @@
     .Carousel-container
       .Carousel-buttons
         svg.Carousel-button.Carousel-button--left(width="40px" height="40px" viewBox="0 0 40 40" @click="turnLeft")
-          path#left-1.Carousel-shape.Carousel-shape--left(:class="index === 1 ? 'is-active' : ''" d="M20,38.8C9.6,38.8,1.2,30.4,1.2,20S9.6,1.2,20,1.2S38.8,9.6,38.8,20S30.4,38.8,20,38.8z")
-          path#left-2.Carousel-shape.Carousel-shape--left(:class="index === 2 ? 'is-active' : ''" d="M38.8,33.6H1.2L20,1.1L38.8,33.6z")
-          path#left-3.Carousel-shape.Carousel-shape--left(:class="index === 3 ? 'is-active' : ''" d="M20,1.2L38.8,20L20,38.8L1.2,20L20,1.2z")
-          path#left-0.Carousel-shape.Carousel-shape--left(:class="index === 0 ? 'is-active' : ''" d="M38.8,14.8l-7.2,22H8.4l-7.2-22L20,1.2L38.8,14.8z")
+          path#left-1.Carousel-shape.Carousel-shape--left(:class="index === 1 ? 'is-chosen' : ''" d="M20,38.8C9.6,38.8,1.2,30.4,1.2,20S9.6,1.2,20,1.2S38.8,9.6,38.8,20S30.4,38.8,20,38.8z")
+          path#left-2.Carousel-shape.Carousel-shape--left(:class="index === 2 ? 'is-chosen' : ''" d="M38.8,33.6H1.2L20,1.1L38.8,33.6z")
+          path#left-3.Carousel-shape.Carousel-shape--left(:class="index === 3 ? 'is-chosen' : ''" d="M20,1.2L38.8,20L20,38.8L1.2,20L20,1.2z")
+          path#left-0.Carousel-shape.Carousel-shape--left(:class="index === 0 ? 'is-chosen' : ''" d="M38.8,14.8l-7.2,22H8.4l-7.2-22L20,1.2L38.8,14.8z")
+          path#left.Carousel-shape.is-active
           polyline.Carousel-shape.is-active(points="25,22.5 20,17.5 15,22.5")
         svg.Carousel-button.Carousel-button--right(width="40px" height="40px" viewBox="0 0 40 40" @click="turnRight")
-          path#right-3.Carousel-shape.Carousel-shape--right(:class="index === 3 ? 'is-active' : ''" d="M20,38.8C9.6,38.8,1.2,30.4,1.2,20S9.6,1.2,20,1.2S38.8,9.6,38.8,20S30.4,38.8,20,38.8z")
-          path#right-0.Carousel-shape.Carousel-shape--right(:class="index === 0 ? 'is-active' : ''" d="M38.8,33.6H1.2L20,1.1L38.8,33.6z")
-          path#right-1.Carousel-shape.Carousel-shape--right(:class="index === 1 ? 'is-active' : ''" d="M20,1.2L38.8,20L20,38.8L1.2,20L20,1.2z")
-          path#right-2.Carousel-shape.Carousel-shape--right(:class="index === 2 ? 'is-active' : ''" d="M38.8,14.8l-7.2,22H8.4l-7.2-22L20,1.2L38.8,14.8z")
+          path#right-3.Carousel-shape.Carousel-shape--right(:class="index === 3 ? 'is-chosen' : ''" d="M20,38.8C9.6,38.8,1.2,30.4,1.2,20S9.6,1.2,20,1.2S38.8,9.6,38.8,20S30.4,38.8,20,38.8z")
+          path#right-0.Carousel-shape.Carousel-shape--right(:class="index === 0 ? 'is-chosen' : ''" d="M38.8,33.6H1.2L20,1.1L38.8,33.6z")
+          path#right-1.Carousel-shape.Carousel-shape--right(:class="index === 1 ? 'is-chosen' : ''" d="M20,1.2L38.8,20L20,38.8L1.2,20L20,1.2z")
+          path#right-2.Carousel-shape.Carousel-shape--right(:class="index === 2 ? 'is-chosen' : ''" d="M38.8,14.8l-7.2,22H8.4l-7.2-22L20,1.2L38.8,14.8z")
+          path#right.Carousel-shape.is-active
           polyline.Carousel-shape.is-active(points="25,22.5 20,17.5 15,22.5")
       .Carousel-content
         router-link.Carousel-item(v-bind:class="`Carousel-item--${modulo(range, 4)}`" :to="{ name: 'pocketcare' }")
@@ -44,14 +46,14 @@ export default {
       return ((n % m) + m) % m;
     },
     turnLeft: function() {
-      const tweenLeft = kute.to('.Carousel-shape--left.is-active', { path: `#left-${this.modulo(this.range - 1, 4) }` })
-      const tweenRight = kute.to(`.Carousel-shape--right.is-active`, { path: `#right-${this.modulo(this.range - 1, 4) }` })
+      const tweenLeft = kute.to('#left', { path: `#left-${this.modulo(this.range - 1, 4) }` })
+      const tweenRight = kute.to(`#right`, { path: `#right-${this.modulo(this.range - 1, 4) }` })
       tweenLeft.start()
       tweenRight.start()
     },
     turnRight: function() {
-      const tweenLeft = kute.to('.Carousel-shape--left.is-active', { path: `#left-${this.modulo(this.range + 1, 4) }` })
-      const tweenRight = kute.to(`.Carousel-shape--right.is-active`, { path: `#right-${this.modulo(this.range + 1, 4) }` })
+      const tweenLeft = kute.to('#left', { path: `#left-${this.modulo(this.range + 1, 4) }` })
+      const tweenRight = kute.to(`#right`, { path: `#right-${this.modulo(this.range + 1, 4) }` })
       tweenLeft.start()
       tweenRight.start()
     }
@@ -60,6 +62,8 @@ export default {
     this.index = this.range
   },
   mounted() {
+    this.$el.querySelector('#left').setAttribute('d', this.$el.querySelector(`#left-${this.index}`).getAttribute('d'))
+    this.$el.querySelector('#right').setAttribute('d', this.$el.querySelector(`#right-${this.index}`).getAttribute('d'))
   }
 }
 </script>
