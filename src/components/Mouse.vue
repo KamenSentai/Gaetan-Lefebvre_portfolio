a23af1f105815733c83145960a2c3d50aa38bd8f
AlainKRSS <cvt.alain@gmail.com>
2019-03-08 22:33:48 +0100

    fix(cursor): fix cursor position and style

--- src/components/Mouse.vue
+++ src/components/Mouse.vue
@@ -27,19 +27,27 @@ export default {
       this.className = 'is-hidden'
     } else {
       const _body = document.body
+      let screen = { x: window.innerWidth / 2, y: window.innerHeight / 2 }
       let mouse = { x: window.innerWidth / 2, y: window.innerHeight / 2 }
       let position = { x: window.innerWidth / 2, y: window.innerHeight / 2 }
 
       window.addEventListener('mousemove', event => {
+        screen.x = event.clientX
+        screen.y = event.clientY
         mouse.x = event.clientX - _body.getBoundingClientRect().left
         mouse.y = event.clientY - _body.getBoundingClientRect().top
+      })
+
+      window.addEventListener('scroll', () => {
+        mouse.x = screen.x - _body.getBoundingClientRect().left
+        mouse.y = screen.y - _body.getBoundingClientRect().top
       })
 
       const animateCircle = () => {
         position.x += (mouse.x - position.x) * .125
         position.y += (mouse.y - position.y) * .125
 
-        _mousePointer.style.transform = `translate(${mouse.x}px, ${mouse.y}px)`
+        _mousePointer.style.transform = `translate(${position.x}px, ${position.y}px)`
         _mouseCircle.style.transform = `translate(${position.x}px, ${position.y}px)`
         window.requestAnimationFrame(animateCircle)
       }
@@ -84,7 +92,7 @@ export default {
     width: .8rem;
     height: .8rem;
     background-color: $white;
-    box-shadow: 0 0 .8rem $black;
+    box-shadow: 0 0 .8rem rgba($black, .75);
   }
 
   &-circle {
