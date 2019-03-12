9274587456d65510710ba8bf9f3b11f7000340ce
AlainKRSS <cvt.alain@gmail.com>
2019-03-12 15:34:29 +0100

    fix(): hide cursor

--- components/Mouse.vue
+++ components/Mouse.vue
@@ -156,6 +156,17 @@ export default {
       ) this.state = 'is-hidden'
       else this.state = 'is-active'
     })
+
+    if (
+      navigator.userAgent.match(/Android/i)
+      || navigator.userAgent.match(/webOS/i)
+      || navigator.userAgent.match(/iPhone/i)
+      || navigator.userAgent.match(/iPad/i)
+      || navigator.userAgent.match(/iPod/i)
+      || navigator.userAgent.match(/BlackBerry/i)
+      || navigator.userAgent.match(/Windows Phone/i)
+    ) this.state = 'is-hidden'
+    else this.state = 'is-active'
   }
 }
 </script>
