74a8b4862366e891ea6cba03852da2470a0bcf1b
AlainKRSS <cvt.alain@gmail.com>
2019-03-17 04:03:28 +0100

    test(): load image

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,9 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      { src: 'https://code.createjs.com/1.0.0/preloadjs.min.js' }
     ]
   },
   /*
