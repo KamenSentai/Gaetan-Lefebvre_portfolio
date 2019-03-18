9b1de87828fd05b5571cb7bae575420eca956792
AlainKRSS <cvt.alain@gmail.com>
2019-03-18 13:10:53 +0100

    fix(): change class name

--- assets/scripts/IndexController.js
+++ assets/scripts/IndexController.js
@@ -21,7 +21,7 @@ class IndexController extends PageController {
       if (!page.$route.params.from || !page.$route.params.from.includes('projects-')) {
         TweenLite.fromTo('.Header-navbar', 1, { opacity: 0 }, { opacity: 1, delay: 2, ease: Power2.easeInOut })
       }
-      TweenLite.fromTo('.Hero-hold', 1, { opacity: 0, x: -30 }, { opacity: 1, x: 0, delay: 2, ease: Power2.easeInOut, onComplete: () => {
+      TweenLite.fromTo('.Hero-push', 1, { opacity: 0, x: -30 }, { opacity: 1, x: 0, delay: 2, ease: Power2.easeInOut, onComplete: () => {
         document.querySelector('.Hero-shape').style.transform = null
         document.body.style.pointerEvents = 'auto'
         done()
@@ -41,7 +41,7 @@ class IndexController extends PageController {
       TweenLite.fromTo('.Hero-front', .5, { opacity: 1 }, { opacity: 0, delay: .25, ease: Power2.easeInOut })
       TweenLite.fromTo('.Hero-description', 1, { opacity: 1, y: 0 }, { opacity: 0, y: 30, delay: 0, ease: Power2.easeInOut })
       TweenLite.fromTo('.Hero-texts', 1, { opacity: 1, y: 0 }, { opacity: 0, y: 30, delay: 0, ease: Power2.easeInOut })
-      TweenLite.fromTo('.Hero-hold', 1, { opacity: 1, x: 0 }, { opacity: 0, x: 30, delay: .5, ease: Power2.easeInOut })
+      TweenLite.fromTo('.Hero-push', 1, { opacity: 1, x: 0 }, { opacity: 0, x: 30, delay: .5, ease: Power2.easeInOut })
       TweenLite.fromTo('.Header-navbar', .5, { opacity: 1 }, { opacity: 0, delay: 1, ease: Power2.easeInOut, onComplete: () => {
         document.body.style.pointerEvents = 'auto'
         done()
