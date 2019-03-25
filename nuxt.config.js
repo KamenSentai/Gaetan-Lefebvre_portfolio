8cce34a480f573189cf1b92f1ab0994b8234bf49
AlainKRSS <cvt.alain@gmail.com>
2019-03-25 20:54:59 +0100

    feat(OG): add meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,18 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'msapplication-TileColor', content: '#141414' },
-      { name: 'theme-color', content: '#141414' }
+      { name: 'theme-color', content: '#141414' },
+      { name: 'og:title', content: 'Gaëtan LEFEBVRE | Interactive Designer & Motion Designer' },
+      { name: 'og:description', content: 'Hello all! I’m looking for a 6th month internship as an Interactive Designer. Today, I’m a 4th year student at HETIC and I’m passionnante about user experience and user interface.' },
+      { name: 'og:type', content: 'website' },
+      { name: 'og:url', content: 'https://gaetanlefebvre.fr/' },
+      { name: 'og:image', content: '/graph/facebook.png' },
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: '@gaetanlefebvre' },
+      { name: 'twitter:title', content: 'Gaëtan LEFEBVRE | Interactive Designer & Motion Designer' },
+      { name: 'twitter:description', content: 'Hello all! I’m looking for a 6th month internship as an Interactive Designer. Today, I’m a 4th year student at HETIC and I’m passionnante about user experience and user interface.' },
+      { name: 'twitter:image', content: '/graph/twitter.png' },
+      { name: 'twitter:image:alt', content: 'Gaëtan LEFEBVRE' }
     ],
     link: [
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
