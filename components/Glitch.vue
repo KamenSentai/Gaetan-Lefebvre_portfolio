fdb1d87ec0a1137f0c2c3ded553a30b09be25590
AlainKRSS <cvt.alain@gmail.com>
2019-03-26 21:41:47 +0100

    fix(): fix glitch lazy load

--- components/Glitch.vue
+++ components/Glitch.vue
@@ -1,11 +1,14 @@
 <template lang="pug">
-img.Glitch(:src="require(`../${path}${src}.png`)"  :alt="capitalizeFirstLetter(alt)" draggable="false")
+.Glitch
+  img.Glitch-original(v-bind:class="!isGlitching ? 'is-active' : ''" v-lazy="require(`~/${path}${image.src}.png`)"  :alt="capitalizeFirstLetter(image.alt)" draggable="false")
+  img.Glitch-effect(v-bind:class="isGlitching ? 'is-active' : ''" :src="require(`../${path}${src}.png`)"  :alt="capitalizeFirstLetter(alt)" draggable="false")
 </template>
 
 <script>
 export default {
   data() {
     return {
+      isGlitching: false,
       number: 0,
       total: 10,
       frequency: 75,
@@ -28,6 +31,7 @@ export default {
   methods: {
     glitchPeriodic() {
       if (this.number < this.total) {
+        this.isGlitching = true
         this.src = `${this.image.src}/${this.image.src}_${this.number}`
         this.alt = `${this.image.src} glitch`
         ++this.number
@@ -35,6 +39,7 @@ export default {
           this.glitchPeriodic()
         }, this.frequency)
       } else {
+        this.isGlitching = false
         this.src = this.image.src
         this.alt = this.image.alt
         this.number = 0
@@ -69,3 +74,18 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.Glitch {
+  &-original,
+  &-effect {
+    display: none;
+    width: 100%;
+    height: auto;
+
+    &.is-active {
+      display: block;
+    }
+  }
+}
+</style>
